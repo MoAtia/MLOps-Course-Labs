bad755ddded6c8288b17edcb346b71786712b737
Mohamed Atia <moatia213@gmail.com>
2025-05-22 14:24:28 +0300

    Adding the monitoring pipeline

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import joblib
-import numpy as np
 import pandas as pd
 import logging
 from typing import Literal
+# from monitoring import instrumentator
+from prometheus_fastapi_instrumentator import Instrumentator, metrics
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.info("Importing libraries done.")
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -35,7 +37,24 @@ except Exception as e:
     logging.error(f"Error loading model or transformer: {e}")
     raise
 
-# Input schema
+
+# # Expose
+# @app.get("/metrics")
+# def metrics_endpoint():
+#     """
+#     Expose the metrics endpoint.
+#     """
+#     return instrumentator.instrument(app).expose(app, include_in_schema=False, should_gzip=True)
+
+# Instrumentator.instrument(app).expose(app, include_in_schema=False, should_gzip=True)
+# # Input schema
+
+logging.info("Creating metrics endpoint.")
+# Creates a metrics endpoint at /metrics
+Instrumentator().instrument(app).expose(app)
+logging.info("Metrics endpoint created.")
+
+
 class CustomerData(BaseModel):
     CreditScore: float
     Age: float
@@ -63,13 +82,14 @@ def health():
 
 @app.post("/predict")
 def predict(data: CustomerData):
-    logging.info(f"Received data for prediction: {data.dict()}")
+    logging.info(f"Received data for prediction: {data.model_dump()}")
 
     try:
         # Create input in correct order
         input_data = [[getattr(data, col) for col in all_features]]
         logging.info(f"Ordered input data: {input_data}")
         input_data = pd.DataFrame(input_data, columns=all_features)
+        
         # Transform input
         transformed = transformer.transform(input_data)
         logging.info("Data transformed successfully.")
@@ -79,10 +99,14 @@ def predict(data: CustomerData):
         prob = model.predict_proba(transformed)[0][1]
         logging.info(f"Prediction: {prediction[0]}, Probability: {prob}")
 
+        headers = {
+            "churn_probability": round(prob, 4)
+        }
+
         return {
             "churn_prediction": int(prediction[0]),
             "churn_probability": round(prob, 4)
-        }
+        }, 200, headers
 
     except Exception as e:
         logging.error(f"Prediction error: {e}")
